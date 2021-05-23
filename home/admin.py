bfe499c9ab2f53a9e4ac74592e4a0fd31c608846
Nicholas Sokol <nsokol@nicholass-mbp.lan>
2021-05-23 11:30:54 -0700

    version 4

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Bio
 
 # Register your models here.
+
+
+
+@admin.register(Bio)
+class BioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title', 'bioImage', 'bioDesc', 'datetime')
